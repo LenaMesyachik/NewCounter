c22c70ffe2b764cb706761cfee35b740d514e2c9
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-13 13:48:48 +0300

    step 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-
-import React from "react"
+import React, {useState} from "react"
 
 
 const App = () => {
+
+    const [counter, setCounter] = useState(0)
+    const [minCounter, setMinCounter] = useState(0)
+    const [maxCounter, setMaxCounter] = useState(5)
+    const addCounter = () => {
+        if (counter < maxCounter)
+        setCounter(counter + 1)
+    }
+    const resetCount = () => {
+        setCounter(minCounter)
+    }
     return (
         <div>
-            Hello
+            {counter}
+            <div>
+                <button onClick={addCounter}></button>
+                <button onClick={resetCount}></button>
+            </div>
         </div>
+
     )
 }
 
