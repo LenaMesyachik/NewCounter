528a34ba902eb95a67e02faa9d690cdb6c4b074d
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-23 20:22:34 +0300

    step 5 useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,19 @@ const App = () => {
     const [counter, setCounter] = useState(0)
     const [minCounter, setMinCounter] = useState(0)
     const [maxCounter, setMaxCounter] = useState(5)
+
+
     useEffect(() => {
         const valueString = localStorage.getItem('counterValue')
+        console.log('OK')
         if (valueString) {
+            console.log('OK2')
             let newValue = JSON.parse(valueString)
             setCounter(newValue)
         }
     }, [])
     useEffect(() => {
+        console.log('OK GO')
         localStorage.setItem('counterValue', JSON.stringify(counter))
     }, [counter])
 
